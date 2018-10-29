b44257445958168850e8671e5dfd0bed79003c81
Flannon Jackson <flannon@nyu.edu>
2018-10-29 08:34:10 -0400

    Rewrote condition to add uid

--- bin/getuid.sh
+++ bin/getuid.sh
@@ -5,8 +5,13 @@
 usermod -a -G mock $(id -u)
 
 # Add default uid to /etc/passwd
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
+#if ! whoami &> /dev/null; then
+#  if [ -w /etc/passwd ]; then
+#    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+#  fi
+#fi
+
+[[ ! $(whoami 2> /dev/null) ]] && \
+  [[ -w /etc/passwd ]] && \
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} \
+    user:${HOME}:/sbin/nologin" >> /etc/passwd
